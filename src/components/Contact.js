80b54054090781e910a0a9c764c6e1d2d3cdac35
rollpz <rolandlopez.developer@gmail.com>
2020-03-17 14:08:56 +0100

    creating modals !

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Modal from './Modal'
 
 const Contact = (props) => {
     
     return(
         <div>
+            <Modal/>
             <div className="ui raised very padded text container segment" style={{marginTop: '80px'}}>
-                <Link to="/card/alex" className='ui header'>Contact of Alex</Link>
+                <Link to="/alex" className='ui header'>Contact of Alex</Link>
                 <p>Blabla.....</p>
             </div>
             <div className="ui raised very padded text container segment" style={{marginTop: '80px'}}>
-                <Link to="/card/will" className='ui header'>Contact of Will</Link>
+                <Link to="/will" className='ui header'>Contact of Will</Link>
                 <p>Blabla.....</p>
             </div>
         </div>
